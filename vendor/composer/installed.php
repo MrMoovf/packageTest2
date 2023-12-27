32b44184e5c2fa82bbe0b9b4ce8aa354e3c1840c
MrMoovf <anderskozuch1998@gmail.com>
2023-12-27 16:50:48 +0100

    dd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,20 +1,20 @@
 <?php return array(
     'root' => array(
         'name' => 'arostech/test2',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
-        'type' => 'project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '08f00eecf20bda1d2fc1a4f9b1fe72121946dd25',
+        'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
         'arostech/test2' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
-            'type' => 'project',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '08f00eecf20bda1d2fc1a4f9b1fe72121946dd25',
+            'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
